1d85668fe07a804a4ed589bb7ae8c5c75fb32948
VLTHellolin <i@hellolin.top>
2025-02-12 22:24:56 +0800

    feat: add button sizes

--- packages/preset/src/shortcuts/button.ts
+++ packages/preset/src/shortcuts/button.ts
@@ -1,5 +1,13 @@
 // @unocss-include
-import type { Shortcuts } from '../types';
+import type { Shortcuts, Size, SizeTable } from '../types';
+
+const sizes: SizeTable = {
+  xs: 'px-2 py-1 text-xs',
+  sm: 'px-2.5 py-1.5 text-sm',
+  md: 'px-3 py-2 text-sm',
+  lg: 'px-3.5 py-2.5 text-base rounded-lg',
+  xl: 'px-4.5 py-3 text-lg rounded-lg',
+};
 
 export const button: Shortcuts = [
   ['button', `
@@ -7,10 +15,16 @@ export const button: Shortcuts = [
     relative box-border cursor-pointer
     text-sm font-medium whitespace-nowrap overflow-hidden
     transition-colors rounded-md
-    button-primary button-md
+    button-primary button-size-md
   `],
   ['button-primary', 'bg-primary text-primary-foreground hover:bg-primary/85'],
   ['button-secondary', 'bg-secondary text-secondary-foreground hover:bg-secondary/85'],
   ['button-outline', 'bg-transparent text-accent-foreground hover:bg-accent'],
   ['button-ghost', 'bg-transparent text-accent-foreground hover:(bg-primary text-primary-foreground)'],
+  [/^button-size-(.+)$/, ([, size]) => {
+    if (size in sizes)
+      return sizes[size as Size];
+    return sizes.md;
+  }],
+  ['button-icon', 'h-8 w-8'],
 ];

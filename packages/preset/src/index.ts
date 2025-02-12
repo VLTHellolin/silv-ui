567b1b2cee553626bcc8393fa70b486627dfe75d
VLTHellolin <i@hellolin.top>
2025-02-12 18:12:51 +0800

    chore: hoist unocss dependencies

--- packages/preset/src/index.ts
+++ packages/preset/src/index.ts
@@ -1,7 +1,8 @@
+import type { Theme } from 'unocss/preset-mini';
 import type { PresetSilvOptions } from './types';
 import { definePreset } from 'unocss';
 
-export const presetSilv = definePreset<PresetSilvOptions>(options => {
+export const presetSilv = definePreset<PresetSilvOptions, Theme>(options => {
   return {
     name: '@silv-ui/preset',
     options,
--- packages/preset/src/types.ts
+++ packages/preset/src/types.ts
@@ -1,9 +1,12 @@
+import type { DynamicShortcut, StaticShortcut } from 'unocss';
+import type { Theme } from 'unocss/preset-mini';
+
+export type Shortcut = StaticShortcut | DynamicShortcut<Theme>;
+export type Shortcuts = Shortcut[];
+
 export type Size = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 export type SizeTable = Record<Size, string>;
 
 export interface PresetSilvOptions {
-  colors?: {
-    primary?: string;
-    secondary?: string;
-  };
+  theme?: string;
 }

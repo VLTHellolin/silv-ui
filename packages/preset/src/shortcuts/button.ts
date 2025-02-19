4ef4b069d803d8c3ac2932997968d4b73a3bd044
VLTHellolin <i@hellolin.top>
2025-02-19 18:45:10 +0800

    feat: write button variants using hue utils

--- packages/preset/src/shortcuts/button.ts
+++ packages/preset/src/shortcuts/button.ts
@@ -1,32 +1,23 @@
 // @unocss-include
-import type { Shortcuts, Size, SizeTable } from '../types';
-
-const sizes: SizeTable = {
-  xs: 'px-2 py-1 text-xs',
-  sm: 'px-2.5 py-1.5 text-sm',
-  md: 'px-3 py-2 text-sm',
-  lg: 'px-3.5 py-2.5 text-base rounded-lg',
-  xl: 'px-4.5 py-3 text-lg rounded-lg',
-};
+import type { Shortcuts } from '../types';
 
 export const button: Shortcuts = [
   ['button', `
     inline-flex items-center justify-center gap-2 children:shrink-0
     relative box-border cursor-pointer
     text-sm font-medium whitespace-nowrap overflow-hidden
-    transition-colors rounded-md
+    rounded-md transition-colors motion-reduce:transition-none
     disabled:(cursor-not-allowed pointer-events-none opacity-70 shadow-none)
-    button-primary button-size-md
+    hue-primary button-md button-solid
   `],
-  ['button-primary', 'bg-primary text-primary-foreground hover:bg-primary/85'],
-  ['button-secondary', 'bg-secondary text-secondary-foreground hover:bg-secondary/85'],
-  ['button-outline', 'bg-transparent text-accent-foreground border-(1 solid primary) hover:bg-accent'],
-  ['button-light', 'bg-transparent text-secondary-foreground hover:bg-secondary/85'],
-  ['button-ghost', 'bg-transparent text-accent-foreground hover:(bg-primary text-primary-foreground)'],
-  [/^button-size-(.+)$/, ([, size]) => {
-    if (size in sizes)
-      return sizes[size as Size];
-    return sizes.md;
-  }],
+  ['button-xs', 'px-2 py-1 text-xs'],
+  ['button-sm', 'px-2.5 py-1.5 text-sm'],
+  ['button-md', 'px-3 py-2 text-sm'],
+  ['button-lg', 'px-3.5 py-2.5 text-base rounded-lg'],
+  ['button-xl', 'px-4.5 py-3 text-lg rounded-lg'],
   ['button-icon', 'h-8 w-8'],
+  ['button-solid', 'text-hue-fg bg-hue9 hover:bg-hue10'],
+  ['button-outline', 'text-hue11 bg-transparent ring-(1 inset hue7) hover:(ring-hue8 bg-hue4)'],
+  ['button-soft', 'text-hue11 bg-hue3 hover:bg-hue4'],
+  ['button-ghost', 'text-hue11 bg-transparent hover:(text-hue-fg bg-hue4)'],
 ];
--- packages/preset/uno.config.ts
+++ packages/preset/uno.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig, presetUno, transformerVariantGroup } from 'unocss';
+import { presetRadix, radixColors } from 'unocss-preset-radix';
 
 export default defineConfig({
   presets: [
     presetUno(),
+    presetRadix({
+      palette: radixColors,
+      aliases: {
+        primary: 'violet',
+      },
+      prefix: '--silv-',
+      variantPrefix: 'silv-',
+      darkSelector: '.dark, .dark-theme',
+      lightSelector: ':root, .light, .light-theme',
+    }),
   ],
   transformers: [
     transformerVariantGroup(),

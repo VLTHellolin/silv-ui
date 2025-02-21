90cc396e6a1868f454ae04cabd8fd8a37dd2308f
VLTHellolin <i@hellolin.top>
2025-02-22 00:54:17 +0800

    build: make the build pass

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -1,13 +1,11 @@
 import { resolve } from 'node:path';
 import react from '@vitejs/plugin-react-swc';
-import unocss from 'unocss/vite';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   plugins: [
     react(),
-    unocss(),
     dts({ rollupTypes: true }),
   ],
   build: {

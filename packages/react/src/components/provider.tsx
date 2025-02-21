dc3090cac8508a32c654e76917278c0fe85c8ec2
VLTHellolin <i@hellolin.top>
2025-02-21 23:53:30 +0800

    feat(react): wrap the direction provider of Radix

--- packages/react/src/components/provider.tsx
+++ packages/react/src/components/provider.tsx
@@ -1,19 +1,25 @@
 import type React from 'react';
 import type { Config } from '../types';
+import { Direction } from 'radix-ui';
 import { ConfigContext } from '../utils/config';
 
 export interface SilvUIProviderProps {
   children?: React.ReactNode;
   config?: Config;
+  dir?: 'ltr' | 'rtl';
 }
 
 export function SilvUIProvider({
   children,
   config,
+  dir,
 }: SilvUIProviderProps) {
   return (
     <ConfigContext value={config ?? {}}>
-      {children}
+      {/* eslint-disable-next-line react/no-context-provider */}
+      <Direction.Provider dir={dir ?? 'ltr'}>
+        {children}
+      </Direction.Provider>
     </ConfigContext>
   );
 }
